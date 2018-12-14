8351d0da5d991dec01db37227b139ed595d50bb2
seanquevedo <squevedo@csumb.edu>
2018-12-13 18:57:04 -0800

    Created service to change stock in TicketsDB

--- src/main/java/cst438team17/ticketsdb/HeroController.java
+++ src/main/java/cst438team17/ticketsdb/HeroController.java
@@ -1,10 +1,14 @@
 package cst438team17.ticketsdb;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cst438team17.ticketsdb.entities.ConcertTicket;
@@ -25,5 +29,19 @@ public class HeroController {
         ConcertTicket result = ticketRepo.findByRepoId(id);
         return result;
     }
+
+    @PutMapping ("/id/{id}")
+    public ResponseEntity<ConcertTicket> updateTicket(@PathVariable("id") String id, @RequestBody ConcertTicket ticket) {
+ 
+		Optional<ConcertTicket> ticketData = ticketRepo.findById(id);
+ 
+		if (ticketData.isPresent()) {
+            ConcertTicket _ticket = ticketData.get();
+            _ticket.setStock(ticket.getStock());
+			return new ResponseEntity<>(ticketRepo.save(_ticket), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 
 }
